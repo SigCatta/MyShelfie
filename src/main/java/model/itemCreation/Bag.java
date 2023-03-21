cc4193e17b83083ee7aa218e555ec60794c14bc9
lucca <simone.lucca@mail.polimi.it>
2023-03-21 21:58:47 +0100

    Added Bag, Color, ItemTile

---
+++ src/main/java/model/itemCreation/Bag.java
@@ -0,0 +1,64 @@
+package model.itemCreation;
+
+import model.gameItems.Tiles.ItemTile;
+import model.gameItems.Tiles.Color;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Bag {
+    private final int NUMBER_OF_COLORS;
+
+    private int tileCount = 132;
+    private final HashMap<Color, Integer> colorNumber = new HashMap<>();
+
+    public Bag(){
+
+        NUMBER_OF_COLORS = Color.values().length;
+
+        for(Color color : Color.values()){
+            colorNumber.put(color, tileCount/NUMBER_OF_COLORS);
+        }
+    }
+
+
+
+    public ArrayList<ItemTile> drawItemTiles(int numberOfTiles){
+
+        if(numberOfTiles > tileCount){
+            for(Color color : Color.values()){
+                colorNumber.put(color, (numberOfTiles - tileCount)/NUMBER_OF_COLORS );
+            }
+        }
+
+        ArrayList<ItemTile> itemTileList = new ArrayList<>();
+
+        for(int i = 0; i < numberOfTiles; i++){
+            itemTileList.add(new ItemTile(randomColor()));
+        }
+
+        return itemTileList;
+    }
+
+    public ItemTile drawSingleTiles(){
+        return new ItemTile(randomColor());
+    }
+
+    private Color randomColor(){
+
+        double random = Math.random() * tileCount;
+        tileCount--;
+
+        int partialCount = 0;
+        for(Color color : Color.values()){
+
+            partialCount += colorNumber.get(color);
+
+            if(partialCount >= random){
+                return color;
+            }
+        }
+
+        return Color.BLUE;
+    }
+}
