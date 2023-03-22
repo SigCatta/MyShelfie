4444cfac1431a18cf51178d5ee67328802baf241
Luca Cattani <luca.cattani@mail.polimi.it>
2023-03-22 22:31:32 +0100

    Started integrating JSON support

---
+++ src/main/java/ReadFromJSONFile.java
@@ -0,0 +1,21 @@
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFromJSONFile {
+    JSONParser jsonParser;
+
+    public ReadFromJSONFile() {
+        this.jsonParser = new JSONParser();
+    }
+
+    public void readJSON(String fileName) throws IOException, ParseException {
+        FileReader fileReader = new FileReader(fileName);
+        Object obj = jsonParser.parse(fileReader);
+
+    }
+
+}
--- src/main/java/it/polimi/ingsw/model/cards/personalGoals/PersonalCardDealer.java
+++ src/main/java/it/polimi/ingsw/model/cards/personalGoals/PersonalCardDealer.java
@@ -1,12 +1,17 @@
 package it.polimi.ingsw.model.cards.personalGoals;
 
+import org.json.simple.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class PersonalCardDealer {
-    public static List<PersonalGoal> getCards(int numOfPlayers){
+    private JSONObject jsonObject;
+
+    public static List<PersonalGoal> getCards(int numOfPlayers) {
         List<PersonalGoal> drawnCards = new ArrayList<>();
         //TODO implement random draw from json db
         return drawnCards;
     }
+
 }
