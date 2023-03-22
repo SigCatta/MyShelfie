171b5797b0d7432cd3ac31284c9f2496ff7e793a
Luca Cattani <luca.cattani@mail.polimi.it>
2023-03-22 18:39:07 +0100

    started implementing personal goals

--- src/main/java/it/polimi/ingsw/model/cards/personalGoals/PersonalGoal.java
+++ src/main/java/it/polimi/ingsw/model/cards/personalGoals/PersonalGoal.java
@@ -1,4 +1,36 @@
 package it.polimi.ingsw.model.cards.personalGoals;
 
+import it.polimi.ingsw.model.player.Player;
+import it.polimi.ingsw.model.player.Shelf;
+import it.polimi.ingsw.model.tiles.Color;
+import it.polimi.ingsw.model.tiles.ItemTile;
+
+import java.awt.Point;
+import java.util.HashMap;
+
 public class PersonalGoal {
+    HashMap<Color, Point> achievements;
+    private final Player player;
+
+    protected PersonalGoal(Player player){
+        //TODO implement map fillout from json
+
+        this.player = player;
+    }
+
+    public int calculateScore(){
+        int score = 0;
+        if (!achievements.isEmpty()){
+            for(Color color : Color.values()){
+                Point point = achievements.get(color);
+                Shelf shelf = player.getShelf();
+                ItemTile tileAtPoint = shelf.getTileAtLocation(point);
+                if (tileAtPoint.getColor() == color){
+                    //TODO score += non mi ricordooo
+                    achievements.remove(color);
+                }
+            }
+        }
+        return score;
+    }
 }
