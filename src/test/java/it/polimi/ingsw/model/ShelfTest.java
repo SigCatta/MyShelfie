9e4053232ae548f3eb8a56224b28912c14d51a2a
Arianna Zerbini <arianna.zerbini@gmail.com>
2023-03-22 07:42:51 +0100

    Player, Shelf, ShelfManager created

--- src/test/java/it/polimi/ingsw/model/ShelfTest.java
+++ src/test/java/it/polimi/ingsw/model/ShelfTest.java
@@ -11,21 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShelfTest {
 
-    private Shelf shelf;
-    private int ROWS_SIZE;
-    private int COLUMNS_SIZE;
 
-    @BeforeAll
-    @Test
-    public void testInizialization() {
-
-
-    }
-
-    @Test
-    public void getNumOfBoxLeftInColTest() {
-
-    }
 
 
 }
