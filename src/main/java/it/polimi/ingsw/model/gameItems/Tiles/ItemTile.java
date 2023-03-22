068d876cee8be78640a776a6cf06321419fd8a22
Arianna Zerbini <arianna.zerbini@gmail.com>
2023-03-22 07:05:26 +0100

    Player, Shelf, ShelfManager created

---
+++ src/main/java/it/polimi/ingsw/model/gameItems/Tiles/ItemTile.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model.gameItems.Tiles;
+
+public class ItemTile {
+    private final Color COLOR;
+
+    public ItemTile(Color color){
+        this.COLOR = color;
+        //request for the tile image from database
+    }
+
+    public Color getColor() {
+        return COLOR;
+    }
+
+}
---
+++ src/test/java/model/cardCreationTest/CommonGoalTest.java
@@ -0,0 +1,4 @@
+package model.cardCreationTest;
+
+public class CommonGoalTest {
+}
