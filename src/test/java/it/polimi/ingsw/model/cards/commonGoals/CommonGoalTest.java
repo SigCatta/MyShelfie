67744858fa2e660c9457aa9d2bd8546d8ee3df3b
Luca Cattani <luca.cattani@mail.polimi.it>
2023-03-22 16:33:54 +0100

    Fixed dependencies

--- src/test/java/it/polimi/ingsw/model/cards/commonGoals/CommonGoalTest.java
+++ src/test/java/it/polimi/ingsw/model/cards/commonGoals/CommonGoalTest.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.model.cards.commonGoals;
 
-import it.polimi.ingsw.model.gameItems.Tiles.Color;
-import it.polimi.ingsw.model.gameItems.Tiles.ItemTile;
+import it.polimi.ingsw.model.tiles.*;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
