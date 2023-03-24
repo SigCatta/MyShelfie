7b1a9f229d96a3e1368c6e37f4a6ea98d7974cde
lucca <simone.lucca@mail.polimi.it>
2023-03-24 20:03:09 +0100

    Board Refresher, fixed. Used modified design pattern

--- src/main/java/it/polimi/ingsw/model/board/BoardRefresher/RefresherCommandHandler.java
+++ src/main/java/it/polimi/ingsw/model/board/BoardRefresher/RefresherCommandHandler.java
@@ -5,31 +5,39 @@ import it.polimi.ingsw.model.tiles.Bag;
 
 
 public class RefresherCommandHandler {
-    private final boolean[][] pointsToBeFilled = new boolean[9][9];
+    private final boolean[][] pointsToBeFilled;
     private final Board board;
+    private final int BOARD_SIZE;
     private final Bag bag;
 
     RefresherCommandHandler(Board board, Bag bag){
         this.board = board;
         this.bag = bag;
+
+        BOARD_SIZE = board.getBoardGrid().length;
+        pointsToBeFilled = new boolean[BOARD_SIZE][BOARD_SIZE];
     }
 
-    // Add command to the list
-    public void addPointsInformation(BoardLookUpTable command) {
-        if(command.getPointsToBeFilled().length != pointsToBeFilled.length) return;
+    /**
+     * uses the lookUpTable to change the pointsToBeFilled matrix,
+     * this it holds true only in the positions where the tile is needed
+     * @param lookUpTable matrix
+     */
+    public void addPointsInformation(BoardLookUpTable lookUpTable) {
+        if(lookUpTable.getPointsToBeFilled().length != pointsToBeFilled.length) return;
 
-        for (int r = 0; r < pointsToBeFilled.length; r++) {
-            for (int c = 0; c < pointsToBeFilled.length; c++) {
+        for (int r = 0; r < BOARD_SIZE; r++) {
+            for (int c = 0; c < BOARD_SIZE; c++) {
 
-                pointsToBeFilled[r][c] = pointsToBeFilled[r][c] || command.getPointsToBeFilled()[r][c];
+                pointsToBeFilled[r][c] = pointsToBeFilled[r][c] || lookUpTable.getPointsToBeFilled()[r][c];
 
             }
         }
     }
 
     public void executeCommands() {
-        for (int r = 0; r < pointsToBeFilled.length; r++) {
-            for (int c = 0; c < pointsToBeFilled.length; c++) {
+        for (int r = 0; r < BOARD_SIZE; r++) {
+            for (int c = 0; c < BOARD_SIZE; c++) {
 
                 if(pointsToBeFilled[r][c]){
                     board.getBoardGrid()[r][c] = bag.drawSingleTile();
