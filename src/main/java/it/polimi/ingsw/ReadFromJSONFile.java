3131fa40aa8e426c177105a6e3ee13e7e5f5b80a
Luca Cattani <luca.cattani@mail.polimi.it>
2023-03-24 22:02:11 +0100

    added a class to read Personal Goals data from JSON files

--- src/main/java/it/polimi/ingsw/ReadFromJSONFile.java
+++ src/main/java/it/polimi/ingsw/ReadFromJSONFile.java
@@ -1,22 +1,77 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.model.tiles.Color;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
+import java.awt.*;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Stack;
 
 public class ReadFromJSONFile {
-    JSONParser jsonParser;
+    private final JSONParser jsonParser;
+    private final String PATH = "src/data/";
 
+    /**
+     * Contructs a new JSON file reader
+     */
     public ReadFromJSONFile() {
         this.jsonParser = new JSONParser();
     }
 
-    public void readJSON(String fileName) throws IOException, ParseException {
-        FileReader fileReader = new FileReader(fileName);
-        Object obj = jsonParser.parse(fileReader);
+    /**
+     * Reads a JSON file from the src/data/personal_cards/ folder given its name,
+     * if the file exists in the directory the mothod returns a hashmap containing
+     * the color-coordinates pair for each objective
+     *
+     * @param fileName the name of the JSON file that need to be read
+     * @return a map containing the coordinates of each objective, by colour
+     * @throws IOException    if the file does not exist or, for any other reason cannot be opened for reading
+     * @throws ParseException if an incorrect JSON is being parsed
+     * @author Luca Cattani
+     */
+
+    public HashMap<Color, Point> getItemTiles(String fileName) throws IOException, ParseException {
+        HashMap<Color, Point> map = new HashMap<>();
+        FileReader fileReader = new FileReader(PATH + "personal_cards/" + fileName);
+        JSONObject JSONobj = (JSONObject) jsonParser.parse(fileReader);
+
+        for (Color color : Color.values()) {
+            JSONArray arr = (JSONArray) JSONobj.get(color.name());
+            Object x = arr.get(0);
+            Object y = arr.get(1);
+            Point point = new Point(Integer.parseInt(x.toString()), Integer.parseInt(y.toString()));
+            map.put(color, point);
+        }
+
+        return map;
+    }
+
+    /**
+     * Reads the src/data/personal_cards/points.json file to get how many points the player gets for completing
+     * each objective. The ammount of points received is based on the number of objectives already completed
+     * (this data is set while writing the points.json file)
+     *
+     * @return a stack containing the points that is player is going to get for completing each objective
+     * @throws IOException    if the file does not exist or, for any other reason cannot be opened for reading
+     * @throws ParseException if an incorrect JSON is being parsed
+     * @author Luca Cattani
+     */
+    public Stack<Integer> getPointStack() throws IOException, ParseException {
+        Stack<Integer> pointStack = new Stack<>();
+        FileReader fileReader = new FileReader(PATH + "personal_cards/points.json");
+        JSONObject JSONobj = (JSONObject) jsonParser.parse(fileReader);
+
+        JSONArray points = (JSONArray) JSONobj.get("POINTS");
+        for (int i = points.size() - 1; i >= 0; i--) {
+            pointStack.push(Integer.valueOf(points.get(i).toString()));
+        }
+
+        return pointStack;
     }
 
 }
