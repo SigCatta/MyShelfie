e81a43dc6a3aad14b5f646cb7b5a46a16d4b57dd
lucca <simone.lucca@mail.polimi.it>
2023-03-25 12:11:59 +0100

    Board Refresher, fixed.

--- src/main/java/it/polimi/ingsw/model/board/BoardRefresher/BoardLookUpTableFour.java
+++ src/main/java/it/polimi/ingsw/model/board/BoardRefresher/BoardLookUpTableFour.java
@@ -3,15 +3,15 @@ package it.polimi.ingsw.model.board.BoardRefresher;
 public class BoardLookUpTableFour extends BoardLookUpTable {
 
     private final boolean[][] pointsToBeFilled = {
-            {false, false, false, false, true , false, false, false, false},
-            {false, false, false, false, false, true , false, false, false},
-            {false, false, false, false, false, false, false, false, false},
-            {false, true , false, false, false, false, false, false, false},
-            {true , false, false, false, false, false, false, false, true },
-            {false, false, false, false, false, false, false, true , false},
-            {false, false, false, false, false, false, false, false, false},
-            {false, false, false, true , false, false, false, false, false},
-            {false, false, false, false, true , false, false, false, false},
+            {false, false, false, true , true , false, false, false, false},
+            {false, false, false, true , true , true, false, false, false},
+            {false, false, true , true , true , true , true , false, false},
+            {false, true , true , true , true , true , true , true , false},
+            {true , true , true , true , true , true , true , true , true },
+            {true , true , true , true , true , true , true , true , false},
+            {false, false, true , true , true , true , true , false, false},
+            {false, false, false, true , true , true , false, false, false},
+            {false, false, false, false, true , true , false, false, false}
     };
 
     @Override
