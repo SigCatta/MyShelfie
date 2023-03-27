8739b90b02551ecd0d5d8074198a5f7857770928
lucca <simone.lucca@mail.polimi.it>
2023-03-27 17:15:18 +0200

    starting AdjacencyScoreCalculation

--- src/main/java/it/polimi/ingsw/model/board/ScoreCalculation/AdjacencyScoreCalculation.java
+++ src/main/java/it/polimi/ingsw/model/board/ScoreCalculation/AdjacencyScoreCalculation.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.model.board.ScoreCalculation;
 import it.polimi.ingsw.Game;
 import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.model.player.Shelf;
+import it.polimi.ingsw.model.tiles.Color;
 
 public class AdjacencyScoreCalculation {
     //TODO it might be useful to have an interface for the classes that calculate the score
@@ -17,12 +18,18 @@ public class AdjacencyScoreCalculation {
 
     /**
      * calculate the points gained from the adjacency of tiles
-     * in the shelf
+     * in the shelf, if three or more colors touch each other,
+     * then the player should gain points
      * @return points
      */
     public int clalculateScore(){
+        //these variables are set here, so they can change with the player turn
         activePlayer = GAME.getActivePlayer();
         activeShelf = activePlayer.getShelf();
+
+        Color[][] shelfColorRepresentation = activeShelf.generateColorMat();
+
+
 
         //TODO to finish
 
