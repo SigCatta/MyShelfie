bef34034c97260b607de1cb5cf1634c4b11f720e
lucca <simone.lucca@mail.polimi.it>
2023-03-31 09:28:48 +0200

    fix

--- src/main/java/it/polimi/ingsw/model/EndOfTurn/TurnHandler.java
+++ src/main/java/it/polimi/ingsw/model/EndOfTurn/TurnHandler.java
@@ -17,9 +17,7 @@ public class TurnHandler implements EndOfTurnSubject{
 
     public TurnHandler(Game game){
         this.game = game;
-
-        new BoardRefresher(game);
-        new ScoreBoard(game);
+        //TODO set the observer
     }
 
     public void changeTurn(){
