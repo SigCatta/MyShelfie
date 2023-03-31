92a64da3c5fcde37223a01f2ad607d345b9fbf03
lucca <simone.lucca@mail.polimi.it>
2023-03-31 11:25:34 +0200

    bugs fixed

--- src/test/java/it/polimi/ingsw/model/EndOfTurn/RefreshTriggerTest.java
+++ src/test/java/it/polimi/ingsw/model/EndOfTurn/RefreshTriggerTest.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.model.EndOfTurn;
 
-import it.polimi.ingsw.model.EndOfTurn.BoardRefresher.BoardRefresher;
 import it.polimi.ingsw.model.EndOfTurn.BoardRefresher.RefreshTrigger;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.board.Board;
@@ -22,7 +21,7 @@ public class RefreshTriggerTest {
     }
 
     @Test
-    void testNotRefreshable() {
+    void testNotRefreshable1() {
         // Create a board with non isolated tiles
         board1.setItemTile(Color.PINK, 0, 0);
         board1.setItemTile(Color.PINK, 0, 2);
@@ -33,6 +32,20 @@ public class RefreshTriggerTest {
         board1.setItemTile(Color.PINK, 0, 1);
         board1.setItemTile(Color.PINK, 2, 1);
         board1.setItemTile(Color.PINK, 2, 2);
+
+        assertFalse(RefreshTrigger.isBoardRefreshable(board1));
+    }
+
+    @Test
+    void testNotRefreshable2() {
+
+        // Create a board with isolated tiles
+        board1.setItemTile(Color.BLUE, 1, 0);
+        board1.setItemTile(Color.BLUE, 0, 0);
+        board1.setItemTile(Color.LIGHTBLUE, 1, 1);
+        board1.setItemTile(Color.LIGHTBLUE, 1, 2);
+        board1.setItemTile(Color.LIGHTBLUE, 3, 4);
+        board1.setItemTile(Color.LIGHTBLUE, 1, 5);
 
         assertFalse(RefreshTrigger.isBoardRefreshable(board1));
     }
@@ -50,4 +63,6 @@ public class RefreshTriggerTest {
 
         assertTrue(RefreshTrigger.isBoardRefreshable(board1));
     }
+
+
 }
