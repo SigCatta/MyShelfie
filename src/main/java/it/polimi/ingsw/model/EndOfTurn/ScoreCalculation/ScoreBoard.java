62d2086e8b42ad4c00913a93ed37b1d5bac86ba8
Luca Cattani <luca.cattani@mail.polimi.it>
2023-04-02 11:56:00 +0200

    added ScoreBoard deleted FullShelfObserver small refactoring

--- src/main/java/it/polimi/ingsw/model/EndOfTurn/ScoreCalculation/ScoreBoard.java
+++ src/main/java/it/polimi/ingsw/model/EndOfTurn/ScoreCalculation/ScoreBoard.java
@@ -1,33 +1,83 @@
 package it.polimi.ingsw.model.EndOfTurn.ScoreCalculation;
 
+import exceptions.NullItemTileException;
+import exceptions.TooManyCardsRequestedException;
 import it.polimi.ingsw.model.EndOfTurn.EndOfTurnObserver;
 import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.cards.commonGoals.CommonCardDealer;
+import it.polimi.ingsw.model.cards.commonGoals.CommonGoalCard;
 import it.polimi.ingsw.model.player.Player;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ScoreBoard implements EndOfTurnObserver {
 
-    ArrayList<Player> players;
-    HashMap<Player, Integer> pointsPerPlayer;
+    private final ArrayList<Player> players;
+    private final List<CommonGoalCard> commonGoalCards;
+    private Player activePlayer;
+    private boolean isFirstPointAssigned;
 
-    public ScoreBoard(Game game){
+    public ScoreBoard(Game game) throws TooManyCardsRequestedException {
+        this.commonGoalCards = CommonCardDealer.pickCommonGoalCards(2);
         this.players = game.getPlayers();
+        this.activePlayer = game.getActivePlayer();
+        this.isFirstPointAssigned = false;
 
         game.getTurnHandler().attachEndOfTurn(this);
     }
 
-    public void updateScore(){
-        //TODO update the score for the current player
+
+    private void scoreCommonGoalCards(Player player) { // at the end of each turn
+        for (CommonGoalCard commonGoal : commonGoalCards) {
+            int points = commonGoal.calculateScore(player);
+            player.updateScore(points);
+        }
     }
 
     @Override
-    public void update() {
-        updateScore();
+    public void update() throws NullItemTileException { // FULL SHELF
+        scoreCommonGoalCards(activePlayer);
+
+        if (activePlayer.getShelf().isFull()) {
+            if (!isFirstPointAssigned) {
+                scoreFirstCompletedShelf(activePlayer);
+                isFirstPointAssigned = true;
+            }
+        }
+
+        if (isFirstPointAssigned && activePlayer == players.get(players.size() - 1)) {
+            scorePersonalGoals();
+            scoreAdjacency();
+        }
     }
 
-    public void setFirstPlayerToFinish(Player activePlayer) {
-        //TODO: assign 1 point to the player for being the first one to complete his shelf
+
+    private void scoreFirstCompletedShelf(Player firstPlayer) { // only the first time a player fills the shelf
+        if (!isFirstPointAssigned) {
+            isFirstPointAssigned = true;
+            firstPlayer.updateScore(1);
+        }
     }
+
+    private void scorePersonalGoals() throws NullItemTileException { // at the end of the game
+        for (Player player : players) {
+            int points = player.getPersonalGoal().calculateScore();
+            player.updateScore(points);
+        }
+    }
+
+    private void scoreAdjacency() { // at the end of the game
+        for (Player player : players) {
+            int points = AdjacencyScoreCalculation.calculateScore(player);
+            player.updateScore(points);
+        }
+    }
+
+    public Player getWinnter() throws NoSuchElementException {
+        return players.stream().max(Comparator.comparingInt(Player::getScore)).get();
+    }
+
 }
