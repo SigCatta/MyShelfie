48e3cbab90251c083c8379c15839225106a773e5
Luca Cattani <luca.cattani@mail.polimi.it>
2023-04-02 15:57:05 +0200

    added JavaDoc

--- src/main/java/it/polimi/ingsw/model/EndOfTurn/ScoreCalculation/ScoreBoard.java
+++ src/main/java/it/polimi/ingsw/model/EndOfTurn/ScoreCalculation/ScoreBoard.java
@@ -12,30 +12,54 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+/**
+ * The purpouse of this class is to calculate the scores and aassign points to the players whenever thei complete
+ * either a personal or common goal, fill their shelf before the others or place tiles of the same colors
+ * adjacently in thei shelf, it is called at the end of each turn to check if a player can earn any points
+ */
 public class ScoreBoard implements EndOfTurnObserver {
 
     private final ArrayList<Player> players;
     private final List<CommonGoalCard> commonGoalCards;
+    private final List<List<Player>> completedCommonGoal;
     private Player activePlayer;
     private boolean isFirstPointAssigned;
 
+    /**
+     * creates a new scoreboard and assigns it a game
+     */
     public ScoreBoard(Game game) throws TooManyCardsRequestedException {
         this.commonGoalCards = CommonCardDealer.pickCommonGoalCards(2);
         this.players = game.getPlayers();
         this.activePlayer = game.getActivePlayer();
         this.isFirstPointAssigned = false;
-    }
-
-
-    private void scoreCommonGoalCards(Player player) { // at the end of each turn
-        for (CommonGoalCard commonGoal : commonGoalCards) {
-            int points = commonGoal.calculateScore(player);
-            player.updateScore(points);
+        this.completedCommonGoal =  new ArrayList<>();
+        for (CommonGoalCard commonGoal : commonGoalCards){
+            completedCommonGoal.add(new ArrayList<>());
         }
     }
 
+    /**
+     * Checks if the player has completed any new common goals and give him points accordingly
+     */
+    private void scoreCommonGoalCards(Player player) { // at the end of each turn
+        for (int i = 0;i <  commonGoalCards.size(); i++) {
+            if (!completedCommonGoal.get(i).contains(player)){
+                completedCommonGoal.get(i).add(player);
+                CommonGoalCard commonGoal = commonGoalCards.get(i);
+                int points = commonGoal.calculateScore(player);
+                player.updateScore(points);
+            }
+        }
+    }
+
+
+    /**
+     * Called each time a player's turn finishes, it checks if the current player has completed any of the common goals and
+     * assigns points accordingly. It also calculates points for personal goals and tile adjacency if the game is finished
+     */
     @Override
-    public void update() throws NullItemTileException { // FULL SHELF
+    public void update() throws NullItemTileException {
         scoreCommonGoalCards(activePlayer);
 
         if (activePlayer.getShelf().isFull()) {
@@ -49,16 +73,24 @@ public class ScoreBoard implements EndOfTurnObserver {
             scorePersonalGoals();
             scoreAdjacency();
         }
+
+        //TODO call the end of the game
     }
 
-
-    private void scoreFirstCompletedShelf(Player firstPlayer) { // only the first time a player fills the shelf
+    /**
+     * Checks if the player who just finished their turn is the first to complete the shlef,
+     * if that is the case, the player wins an extra point and the endgame procedure is activated
+     */
+    private void scoreFirstCompletedShelf(Player player) { // only the first time a player fills the shelf
         if (!isFirstPointAssigned) {
             isFirstPointAssigned = true;
-            firstPlayer.updateScore(1);
+            player.updateScore(1);
         }
     }
 
+    /**
+     * Assigngs each player of points based on the completion of their personal goal card
+     */
     private void scorePersonalGoals() throws NullItemTileException { // at the end of the game
         for (Player player : players) {
             int points = player.getPersonalGoal().calculateScore();
@@ -66,6 +98,10 @@ public class ScoreBoard implements EndOfTurnObserver {
         }
     }
 
+
+    /**
+     * Assigngs each player of points based on the adjacency of tiles in their shelf
+     */
     private void scoreAdjacency() { // at the end of the game
         for (Player player : players) {
             int points = AdjacencyScoreCalculation.calculateScore(player);
@@ -73,6 +109,12 @@ public class ScoreBoard implements EndOfTurnObserver {
         }
     }
 
+    /**
+     * Checks which player has the highest score and returns it. If two or more players have the same score,
+     * the winner is the one who played last
+     *
+     * @return the winner of the game
+     */
     public Player getWinner() throws NoSuchElementException {
         //noinspection ComparatorMethodParameterNotUsed
         return players.stream().max((p1, p2) -> p1.getScore() > p2.getScore() ? 1 : -1).orElse(null);
