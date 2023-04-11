d75d7ed34ca1b1b5b7573bd09f7d5e436a71f5de
lucca <simone.lucca@mail.polimi.it>
2023-04-11 18:24:06 +0200

    fixes

--- src/main/java/it/polimi/ingsw/Controller/Server/ServerMappable/SendShelfMapper.java
+++ src/main/java/it/polimi/ingsw/Controller/Server/ServerMappable/SendShelfMapper.java
@@ -1,12 +1,14 @@
 package it.polimi.ingsw.Controller.Server.ServerMappable;
 
+import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.board.Board;
+import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.model.player.Shelf;
 import it.polimi.ingsw.model.tiles.ItemTile;
 
 import java.util.HashMap;
 
-public class SendShelfMapper extends ServerMappable{
+public class SendShelfMapper extends ServerMappable {
     /**
      * the map is built from the object board using this standard:
      * <p>
@@ -18,11 +20,17 @@ public class SendShelfMapper extends ServerMappable{
      */
     @Override
     public void map(Object o) {
-        if (!(o instanceof Shelf)) return;//error
-        Shelf shelf = (Shelf) o;
+        if (!(o instanceof Game)) return;//error
+
+        Game game = (Game) o;
+        Player activePlayer = game.getActivePlayer();
+        Shelf shelf = activePlayer.getShelf();
+
         ItemTile[][] shelfGrid = shelf.getShelfGrid();
 
         HashMap<String, String> map = encodeItemTilesGrid(shelfGrid);
+        map.put("NICKNAME", activePlayer.getNickname());
+        map.put("GAMEID", String.valueOf(game.getGameID()));
 
         //TODO send to the server socket
 
