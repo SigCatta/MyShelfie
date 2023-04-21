f102caa2b68e728d6a7a548e236ac2a96e70705c
azerbini01 <arianna.zerbini@mail.polimi.it>
2023-04-21 07:40:32 +0200

    CommandType enum + CommandMapKey enum

---
+++ src/main/java/it/polimi/ingsw/Controller/enums/CommandMapKey.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.Controller.enums;
+
+public enum CommandMapKey {
+    NICKNAME,
+    GAMEID,
+    COMMAND,
+    MESSAGE,
+    NUM_OF_PLAYERS,
+    ROW,
+    TILE_INDEX,
+
+}
