0608f9b245963e66eb44f9d4af16196a6cd02b00
azerbini01 <arianna.zerbini@mail.polimi.it>
2023-04-23 18:47:45 +0200

    changed back maven-compiler-plugin to base version

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,15 @@
-module proj.ingsw.The.Compiler.Coalition {
+open module proj.ingsw.The.Compiler.Coalition {
     requires java.logging;
     requires json.simple;
     requires java.desktop;
+    exports it.polimi.ingsw;
 
     requires javafx.controls;
     requires javafx.fxml;
     requires org.controlsfx.controls;
 
-    opens it.polimi.ingsw.View.GUI to javafx.fxml;
+    //opens it.polimi.ingsw.View.GUI to javafx.fxml;
     exports it.polimi.ingsw.View.GUI;
     exports it.polimi.ingsw.View.GUI.SceneController;
-    opens it.polimi.ingsw.View.GUI.SceneController to javafx.fxml;
+    //opens it.polimi.ingsw.View.GUI.SceneController to javafx.fxml;
 }
