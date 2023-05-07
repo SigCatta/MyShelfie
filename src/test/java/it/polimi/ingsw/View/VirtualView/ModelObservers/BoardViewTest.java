773d332f193ccfd38b52a61e66c185fc50e7cc5a
lucca <simone.lucca@mail.polimi.it>
2023-05-07 14:35:16 +0200

    added numbers to the board and description to the common goals

--- src/test/java/it/polimi/ingsw/View/VirtualView/ModelObservers/BoardViewTest.java
+++ src/test/java/it/polimi/ingsw/View/VirtualView/ModelObservers/BoardViewTest.java
@@ -1,10 +1,22 @@
 package it.polimi.ingsw.View.VirtualView.ModelObservers;
 
+import it.polimi.ingsw.View.CLI.BoardView;
+import it.polimi.ingsw.VirtualModel.BoardRepresentation;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 public class BoardViewTest {
     @BeforeEach
-    public void setup(){
+    public void setup() {
 
+    }
+
+    @Test
+    public void boardViewTest() {
+        BoardRepresentation.getInstance();
+        ArrayList<String> board = new BoardView().getPrint(new ArrayList<>());
+        new BoardView().getPrint(board);
     }
 }
