e49d051497caf225d264fbc22cd0a4d0b335a05d
lucca <simone.lucca@mail.polimi.it>
2023-05-07 15:31:29 +0200

    fixed test

--- src/test/java/it/polimi/ingsw/View/VirtualView/ModelObservers/BoardViewTest.java
+++ src/test/java/it/polimi/ingsw/View/VirtualView/ModelObservers/BoardViewTest.java
@@ -1,11 +1,7 @@
 package it.polimi.ingsw.View.VirtualView.ModelObservers;
 
-import it.polimi.ingsw.View.CLI.BoardView;
-import it.polimi.ingsw.VirtualModel.BoardRepresentation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 
 public class BoardViewTest {
     @BeforeEach
@@ -15,8 +11,6 @@ public class BoardViewTest {
 
     @Test
     public void boardViewTest() {
-        BoardRepresentation.getInstance();
-        ArrayList<String> board = new BoardView().getPrint(new ArrayList<>());
-        new BoardView().getPrint(board);
+
     }
 }
