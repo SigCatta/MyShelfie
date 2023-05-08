76c6f08e146fbecbe066830c335ccddc6ecfa09d
Luca Cattani <luca.cattani@mail.polimi.it>
2023-05-08 18:26:04 +0200

    Added command executor form common_goals

--- src/main/java/it/polimi/ingsw/network/client/InputStates/readers/commandExecutors/CommonGoalCE.java
+++ src/main/java/it/polimi/ingsw/network/client/InputStates/readers/commandExecutors/CommonGoalCE.java
@@ -1,8 +1,13 @@
 package it.polimi.ingsw.network.client.InputStates.readers.commandExecutors;
 
+import it.polimi.ingsw.View.CLI.CommonGoalView;
+
+import java.util.ArrayList;
+
 public class CommonGoalCE implements CommandExecutor {
     @Override
     public void execute() {
-        //TODO implement
+        CommonGoalView cgv = new CommonGoalView();
+        cgv.addDescription(cgv.getPrint(new ArrayList<>())).forEach(System.out::println);
     }
 }
