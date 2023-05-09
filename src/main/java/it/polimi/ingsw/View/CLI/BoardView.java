4b8698e641c031ea15534d0120443271eff99174
Luca Cattani <luca.cattani@mail.polimi.it>
2023-05-09 15:51:37 +0200

    fixed board

--- src/main/java/it/polimi/ingsw/View/CLI/BoardView.java
+++ src/main/java/it/polimi/ingsw/View/CLI/BoardView.java
@@ -23,7 +23,7 @@ public class BoardView implements ViewElement {
         output.add("┌──0─┬──1─┬──2─┬──3─┬──4─┬──5─┬──6─┬──7─┬──8─┐          .");
         int rowNumber = 0;
         for (Color[] row : board) {
-            StringBuilder string = new StringBuilder(rowNumber);
+            StringBuilder string = new StringBuilder(Integer.toString(rowNumber));
             for (Color color : row) {
                 string
                         .append(colorMap.getOrDefault(color, NULL))
