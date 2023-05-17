6b9123f0512100b1616ff4e214d4a9fe60d2f487
Luca Cattani <luca.cattani@mail.polimi.it>
2023-05-17 11:20:58 +0200

    EightTilesSameColorCGS ~ refactored tests

---
+++ src/test/java/it/polimi/ingsw/model/cards/commonGoals/EightTilesSameColorCGSTest.java
@@ -0,0 +1,52 @@
+package it.polimi.ingsw.model.cards.commonGoals;
+
+import it.polimi.ingsw.model.cards.commonGoals.commonGoalsStrategy.EightTilesSameColorCGS;
+import it.polimi.ingsw.model.player.Shelf;
+import it.polimi.ingsw.Enum.Color;
+import it.polimi.ingsw.model.tiles.ItemTile;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class EightTilesSameColorCGSTest {
+    private final EightTilesSameColorCGS cg = new EightTilesSameColorCGS();
+
+    @Test
+    public void isGoalAchievedTest1() {
+        ItemTile[][] mat = new ItemTile[][]{
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {new ItemTile(Color.PINK), new ItemTile(Color.PINK), new ItemTile(Color.PINK), new ItemTile(Color.PINK), null},
+                {new ItemTile(Color.WHITE), new ItemTile(Color.PINK), new ItemTile(Color.PINK), new ItemTile(Color.GREEN), null},
+        };
+        assertFalse(cg.isGoalAchieved(new Shelf(mat)));
+    }
+
+    @Test
+    public void isGoalAchievedTest2() {
+        ItemTile[][] mat = new ItemTile[][]{
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {new ItemTile(Color.PINK), new ItemTile(Color.PINK), new ItemTile(Color.PINK), new ItemTile(Color.BLUE), new ItemTile(Color.WHITE)},
+                {new ItemTile(Color.PINK), new ItemTile(Color.BLUE), new ItemTile(Color.PINK), new ItemTile(Color.GREEN), new ItemTile(Color.WHITE)},
+        };
+        assertFalse(cg.isGoalAchieved(new Shelf(mat)));
+    }
+
+    @Test
+    public void isGoalAchievedTest3() {
+        ItemTile[][] mat = new ItemTile[][]{
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {new ItemTile(Color.WHITE), new ItemTile(Color.PINK), new ItemTile(Color.PINK), new ItemTile(Color.GREEN), null},
+                {new ItemTile(Color.WHITE), new ItemTile(Color.PINK), new ItemTile(Color.PINK), new ItemTile(Color.GREEN), null},
+                {new ItemTile(Color.WHITE), new ItemTile(Color.PINK), new ItemTile(Color.PINK), new ItemTile(Color.GREEN), null},
+                {new ItemTile(Color.WHITE), new ItemTile(Color.PINK), new ItemTile(Color.PINK), new ItemTile(Color.GREEN), null},
+        };
+        assertTrue(cg.isGoalAchieved(new Shelf(mat)));
+    }
+}
