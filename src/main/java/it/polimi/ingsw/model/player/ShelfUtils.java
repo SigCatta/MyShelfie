7d562696f3e2411eb15500d61b79e621fe0439d8
Luca Cattani <luca.cattani@mail.polimi.it>
2023-05-17 15:23:26 +0200

    FourGroupsOfFourCGS ~ added tests, Refactored ShelfUtils

--- src/main/java/it/polimi/ingsw/model/player/ShelfUtils.java
+++ src/main/java/it/polimi/ingsw/model/player/ShelfUtils.java
@@ -6,7 +6,7 @@ import it.polimi.ingsw.model.tiles.ItemTile;
 public class ShelfUtils {
     /**
      * @param shelfGrid the matrix of colors to check, represented as a 2D array of Color objects
-     * @param groupNum the number of separate group to find (at least)
+     * @param groupNum  the number of separate group to find (at least)
      * @param groupSize the number of cells per group
      * @return true if the matrix has at least four groups with size >= 4, false otherwise
      */
@@ -14,38 +14,32 @@ public class ShelfUtils {
         int rows = shelfGrid.length;
         int columns = shelfGrid[0].length;
 
-        // visit matrix and mark visited cells
         boolean[][] visited = new boolean[rows][columns];
         int groupCount = 0;
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 if (shelfGrid[i][j] != null && !visited[i][j]) {
-                    // cell is not null and not visited
                     Color cellColor = shelfGrid[i][j].getColor();
                     int count = dfs(shelfGrid, visited, i, j, cellColor);
                     if (count >= groupSize) {
                         groupCount++;
-                        if (groupCount >= groupNum) {
-                            // at least groupNum groups found
-                            return true;
-                        }
+                        if (groupCount >= groupNum) return true;
                     }
                 }
             }
         }
 
-        // less than groupNum groups found
         return false;
     }
 
     /**
      * Recursive helper method for DFS algorithm to count adjacent cells with the same color.
      *
-     * @param shelfGrid the matrix of colors to check
-     * @param visited boolean matrix to mark visited cells
-     * @param i current row index
-     * @param j current column index
+     * @param shelfGrid the matrix to check
+     * @param visited   boolean matrix to mark visited cells
+     * @param i         current row index
+     * @param j         current column index
      * @param cellColor the color of the cell being visited
      * @return the count of adjacent cells with the same color
      */
@@ -53,7 +47,6 @@ public class ShelfUtils {
         int count = 1;
         visited[i][j] = true;
 
-        // visit adjacent cells
         int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
         for (int[] direction : directions) {
