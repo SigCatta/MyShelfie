0c9077f9107b6c19f270cb1c30652b4acb6ab96f
lucca <simone.lucca@mail.polimi.it>
2023-05-18 20:58:35 +0200

    Added text into the login scene

--- src/main/java/it/polimi/ingsw/View/GUI/SceneController/LoginSceneController.java
+++ src/main/java/it/polimi/ingsw/View/GUI/SceneController/LoginSceneController.java
@@ -1,15 +1,27 @@
 package it.polimi.ingsw.View.GUI.SceneController;
 
 import it.polimi.ingsw.Controller.Client.HandshakeMTS;
+import it.polimi.ingsw.Enum.EchoID;
 import it.polimi.ingsw.VirtualModel.EchosRepresentation;
 import it.polimi.ingsw.VirtualView.Messages.EchoMTC;
 import it.polimi.ingsw.network.client.SocketClient;
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
 
-public class LoginSceneController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginSceneController implements Initializable {
     @FXML
     TextField nicknameField;
 
@@ -24,14 +36,22 @@ public class LoginSceneController {
 
     @FXML
     ImageView wrongNicknameImage;
+    @FXML
+    Text wrongNicknameText;
 
     @FXML
+    AnchorPane nicknameErrorPane;
+    @FXML
     ImageView wrongIpImage;
+    @FXML
+    Text wrongIpText;
+    @FXML
+    StackPane login_scene;
 
     @FXML
     protected void onGoBackButtonClick() {
         //back to the lobby
-        StageController.changeScene("fxml/lobby.fxml","Lobby");
+        StageController.changeScene("fxml/lobby.fxml", "Lobby");
     }
 
     @FXML
@@ -46,20 +66,25 @@ public class LoginSceneController {
             }
         }
 
-        boolean correct;
         EchoMTC message = EchosRepresentation.getInstance().popMessage();
-        if (message.isError()) {
-            correct = false;
-        } else {
-            correct = true;
+        if (message.getID() == EchoID.NICKOK) {
             SocketClient.getInstance().setNickname(getNickname());
+            StageController.changeScene("fxml/enter_game_scene.fxml", "Set number of players");
+        } else if (message.getID() == EchoID.BADNICK) {
+            nicknameErrorPane.setVisible(true);
+            wrongNicknameText.setWrappingWidth(200);
+
+            wrongNicknameText.setText(message.getOutput());
+            FadeTransition fadeTransition = new FadeTransition(Duration.seconds(3), nicknameErrorPane);
+            fadeTransition.setFromValue(1.0);
+            fadeTransition.setToValue(0.0);
+            fadeTransition.play();
         }
-        isNicknameCorrect(correct);
     }
 
     @FXML
     public void setContinueButtonVisible() {
-        if(nicknameField.getText().length()>0 ) {
+        if (nicknameField.getText().length() > 0 && nicknameField.getText().length() < 30) {
             continueButton.setVisible(true);
             continueText.setVisible(true);
         } else {
@@ -69,17 +94,14 @@ public class LoginSceneController {
 
     }
 
-    public void isNicknameCorrect(boolean correct)  {
-        //ckeck if nickname is valid
-        if(correct) {
-            //nickname is correct so change scene
-            wrongNicknameImage.setVisible(false);
-            StageController.changeScene("fxml/enter_game_scene.fxml","Set number of players");
-        } else wrongNicknameImage.setVisible(true);
+    public void onKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            onContinueButtonClick();
+        }
     }
 
-    public void isIpCorrect(boolean correct)  {
-        if(correct) {   //TODO ckeck if ip is valid
+    public void isIpCorrect(boolean correct) {
+        if (correct) {   //TODO ckeck if ip is valid
             wrongIpImage.setVisible(false);   //ip is correct
         } else wrongIpImage.setVisible(true);
     }
@@ -90,5 +112,10 @@ public class LoginSceneController {
 
     public String getIP() {
         return ipField.getText();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        login_scene.setOnKeyPressed(this::onKeyPressed);
     }
 }
